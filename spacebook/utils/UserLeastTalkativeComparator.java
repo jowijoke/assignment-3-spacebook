927a9f50894c83d6ed2b089b19b80d48cf657986
John Kennedy <johnwjken@gmail.com>
2016-05-15 20:07:31 +0100

    Final draft

--- spacebook/utils/UserLeastTalkativeComparator.java
+++ spacebook/utils/UserLeastTalkativeComparator.java
@@ -10,8 +10,10 @@ public class UserLeastTalkativeComparator implements Comparator<User>
   public int compare(User a, User b)
   {
     // Algorithmic code: remove when done
-    // TODO: Complete implementation of method UserLeastTalkativeComparator.compare
-	  // Compare outbox sizes and return integer appropriate to sorting smallest to largest
-    return Integer.compare (a.outbox.size(), b.outbox.size());
+    // TODO: Complete implementation of method
+    // UserLeastTalkativeComparator.compare
+    // Compare outbox sizes and return integer appropriate to sorting smallest
+    // to largest
+    return Integer.compare(a.outbox.size(), b.outbox.size());
   }
 }
